f74396edd7c015d9170ca8bc03d363dad1815ca4
Hellen <yongminni97@gmail.com>
2019-03-27 01:53:05 +0300

    Make one main file with ui. Correct work of 1d and 2d

---
+++ design.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'form.ui',
+# licensing of 'form.ui' applies.
+#
+# Created: Wed Mar 27 00:36:42 2019
+#      by: pyside2-uic  running on PySide2 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(1061, 763)
+        self.loadImageButton = QtWidgets.QPushButton(Form)
+        self.loadImageButton.setGeometry(QtCore.QRect(110, 320, 131, 51))
+        self.loadImageButton.setObjectName("loadImageButton")
+        self.oneDimButton = QtWidgets.QPushButton(Form)
+        self.oneDimButton.setGeometry(QtCore.QRect(470, 310, 131, 51))
+        self.oneDimButton.setObjectName("oneDimButton")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(40, 20, 281, 281))
+        self.label.setFrameShape(QtWidgets.QFrame.Box)
+        self.label.setObjectName("label")
+        self.labelTwo = QtWidgets.QLabel(Form)
+        self.labelTwo.setGeometry(QtCore.QRect(730, 20, 281, 281))
+        self.labelTwo.setFrameShape(QtWidgets.QFrame.Box)
+        self.labelTwo.setObjectName("labelTwo")
+        self.twoDimButton = QtWidgets.QPushButton(Form)
+        self.twoDimButton.setGeometry(QtCore.QRect(810, 310, 131, 51))
+        self.twoDimButton.setObjectName("twoDimButton")
+        self.timeTwoDim = QtWidgets.QTextBrowser(Form)
+        self.timeTwoDim.setGeometry(QtCore.QRect(750, 360, 256, 31))
+        self.timeTwoDim.setObjectName("timeTwoDim")
+        self.labelOne = QtWidgets.QLabel(Form)
+        self.labelOne.setGeometry(QtCore.QRect(380, 20, 291, 281))
+        self.labelOne.setFrameShape(QtWidgets.QFrame.Box)
+        self.labelOne.setObjectName("labelOne")
+        self.timeOneDim = QtWidgets.QTextBrowser(Form)
+        self.timeOneDim.setGeometry(QtCore.QRect(410, 360, 256, 31))
+        self.timeOneDim.setObjectName("timeOneDim")
+        self.tableWidget = QtWidgets.QTableWidget(Form)
+        self.tableWidget.setGeometry(QtCore.QRect(340, 510, 681, 241))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.editSize = QtWidgets.QTextEdit(Form)
+        self.editSize.setGeometry(QtCore.QRect(90, 420, 141, 31))
+        self.editSize.setObjectName("editSize")
+        self.editSigma = QtWidgets.QTextEdit(Form)
+        self.editSigma.setGeometry(QtCore.QRect(90, 460, 141, 31))
+        self.editSigma.setObjectName("editSigma")
+        self.editRadius = QtWidgets.QTextEdit(Form)
+        self.editRadius.setGeometry(QtCore.QRect(90, 500, 141, 31))
+        self.editRadius.setObjectName("editRadius")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(20, 420, 51, 41))
+        self.label_2.setScaledContents(False)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(10, 450, 71, 51))
+        self.label_3.setWordWrap(True)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(10, 490, 61, 41))
+        self.label_4.setWordWrap(True)
+        self.label_4.setObjectName("label_4")
+        self.tableWidget_2 = QtWidgets.QTableWidget(Form)
+        self.tableWidget_2.setGeometry(QtCore.QRect(340, 400, 681, 101))
+        self.tableWidget_2.setObjectName("tableWidget_2")
+        self.tableWidget_2.setColumnCount(0)
+        self.tableWidget_2.setRowCount(0)
+        self.editX = QtWidgets.QTextEdit(Form)
+        self.editX.setGeometry(QtCore.QRect(60, 570, 51, 41))
+        self.editX.setObjectName("editX")
+        self.editY = QtWidgets.QTextEdit(Form)
+        self.editY.setGeometry(QtCore.QRect(150, 570, 51, 41))
+        self.editY.setObjectName("editY")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(40, 580, 55, 31))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(130, 580, 55, 21))
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(80, 540, 111, 31))
+        self.label_7.setObjectName("label_7")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
+        self.loadImageButton.setText(QtWidgets.QApplication.translate("Form", "Load", None, -1))
+        self.oneDimButton.setText(QtWidgets.QApplication.translate("Form", "One", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("Form", "Image 1", None, -1))
+        self.labelTwo.setText(QtWidgets.QApplication.translate("Form", "Two Dim Image", None, -1))
+        self.twoDimButton.setText(QtWidgets.QApplication.translate("Form", "Two", None, -1))
+        self.timeTwoDim.setHtml(QtWidgets.QApplication.translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Time: </p></body></html>", None, -1))
+        self.labelOne.setText(QtWidgets.QApplication.translate("Form", "One Dim Image", None, -1))
+        self.timeOneDim.setHtml(QtWidgets.QApplication.translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Time: </p></body></html>", None, -1))
+        self.label_2.setText(QtWidgets.QApplication.translate("Form", "Размер матрицы", None, -1))
+        self.label_3.setText(QtWidgets.QApplication.translate("Form", "Отклонение (сигма)", None, -1))
+        self.label_4.setText(QtWidgets.QApplication.translate("Form", "Радиус апертуры", None, -1))
+        self.label_5.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-size:12pt;\">Х</span></p></body></html>", None, -1))
+        self.label_6.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-size:12pt;\">Y</span></p></body></html>", None, -1))
+        self.label_7.setText(QtWidgets.QApplication.translate("Form", "<html><head/><body><p><span style=\" font-size:11pt;\">Координаты</span></p></body></html>", None, -1))
+
--- main.py
+++ main.py
@@ -2,50 +2,157 @@ import numpy as np
 import cv2
 import math
 import matplotlib.pyplot as plt
-
-img = cv2.imread('cameraman.bmp', cv2.IMREAD_GRAYSCALE)
-img_out = img.copy()
-
-height = img.shape[0]
-width = img.shape[1]
-
-size = 5
-radius = 1
-sigma = 0.000001
-
-gauss = np.zeros((size,size))
-for i in range(size):
-    for j in range(size):
-        gauss[i][j] = (1/math.sqrt(2*math.pi*sigma))*math.exp(-((i-radius-1)**2 + (j-radius-1)**2)/2*sigma**2)
-gauss = gauss / gauss.sum()
-for i in range(len(gauss)):
-    for j in range(len(gauss[i])):
-        print(gauss[i][j], end=' ')
-    print()
+import sys
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QLabel
+from PySide2.QtWidgets import QTableWidget, QTableWidgetItem
+from PySide2.QtCore import QTranslator
+import design
+import os
+import time
 
 
-print(sum(sum(gauss)))
-
-plt.imshow(gauss, cmap=plt.get_cmap('jet'), interpolation='nearest')
-plt.colorbar()
-plt.show()
+img_name = 'cameraman.bmp'
+img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
 
 
-for i in np.arange(2, height-2):
-    for j in np.arange(2, width-2):        
-        sum = 0
-        for k in np.arange(-2, 3):
-            for l in np.arange(-2, 3):
-                a = img.item(i+k, j+l)
-                p = gauss[2+k, 2+l]
-                sum = sum + (p * a)
-        b = sum
-        img_out.itemset((i,j), b)
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        img_name = self.loadImageButton.clicked.connect(self.loadImage)
+        self.twoDimButton.clicked.connect(self.twoDimBlur)
+        self.oneDimButton.clicked.connect(self.oneDimBlur)
 
-newName = 'sigma'+str(sigma)+'.bmp'
-cv2.imwrite(newName, img_out)
+        self.editSize.setText("5")
+        self.editRadius.setText("1")
+        self.editSigma.setText("0.0000001")
 
-cv2.imshow('image',img_out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    def loadImage(self):
+        self.label.setPixmap(img_name)
+        # directory = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите папку")[0]
+        # self.label.setPixmap(str(directory)) 
+        # return str(directory)
+
+
+    def twoDimBlur(self):
+        img_out = img.copy()
+        height = img.shape[0]
+        width = img.shape[1]
+
+        size = int(self.editSize.toPlainText())
+        radius = float(self.editRadius.toPlainText())
+        sigma = float(self.editSigma.toPlainText())
+
+        gauss = np.zeros((size,size))
+        for i in range(size):
+            for j in range(size):
+                gauss[i][j] = (1/math.sqrt(2*math.pi*sigma))*math.exp(-((i-radius-1)**2 + (j-radius-1)**2)/2*sigma**2)
+        gauss = gauss / gauss.sum()
+        self.tableWidget.setRowCount(size)
+        self.tableWidget.setColumnCount(size)
+
+        for i in range(len(gauss)):
+            for j in range(len(gauss[i])):
+                newItem = QTableWidgetItem(str(gauss[i][j]))
+                self.tableWidget.setItem(i, j, newItem)
+
+        start_time = time.process_time()
+        for i in np.arange(2, height-2):
+            for j in np.arange(2, width-2):        
+                sum = 0
+                for k in np.arange(-2, 3):
+                    for l in np.arange(-2, 3):
+                        a = img.item(i+k, j+l)
+                        p = gauss[2+k, 2+l]
+                        sum = sum + (p * a)
+                b = sum
+                img_out.itemset((i,j), b)
+        end_time = time.process_time()
+        self.timeTwoDim.setText("Time: " + str(end_time - start_time))
+        newName = 'sigma'+str(sigma)+'.bmp'
+        cv2.imwrite(newName, img_out)
+        self.labelTwo.setPixmap(newName)
+        
+
+    def oneDimBlur(self):
+        img_out = img.copy()
+        height = img.shape[0]
+        width = img.shape[1]
+        
+        size = int(self.editSize.toPlainText())
+        radius = float(self.editRadius.toPlainText())
+        sigma = float(self.editSigma.toPlainText())
+        gauss = np.zeros((1,size))
+        for i in range(1):
+            for j in range(size):
+                gauss[i][j] = (1/math.sqrt(2*math.pi*sigma))*math.exp(-((i-radius-1)**2 + (j-radius-1)**2)/2*sigma**2)
+        gauss = gauss / gauss.sum()
+
+        self.tableWidget_2.setRowCount(1)
+        self.tableWidget_2.setColumnCount(size)       
+
+        for i in range(len(gauss)):
+            for j in range(len(gauss[i])):
+                newItem = QTableWidgetItem(str(gauss[i][j]))
+                self.tableWidget_2.setItem(i, j, newItem) 
+                gauss_trans = np.zeros((size,1))
+        for i in range(size):
+            for j in range(1):
+                gauss_trans[i][j] = (1/math.sqrt(2*math.pi*sigma))*math.exp(-((i-radius-1)**2 + (j-radius-1)**2)/2*sigma**2)
+        gauss_trans = gauss_trans / gauss_trans.sum()
+
+
+        start_time = time.process_time()
+        for i in np.arange(height):
+            for j in np.arange(width-5):        
+                sum = 0
+                for k in np.arange(1):
+                    for l in np.arange(size):
+                        a = img.item(i+k, j+l)
+                        p = gauss[0,l]
+                        sum = sum + (p * a)
+                b = sum
+                img_out.itemset((i,j), b)
+
+        for i in np.arange(height-5):
+            for j in np.arange(width):        
+                sum = 0
+                for k in np.arange(-2, 3):
+                    for l in np.arange(1):
+                        a = img_out.item(i+k, j+l)
+                        p = gauss_trans[l,0]
+                        sum = sum + (p * a)
+                b = sum
+                img_out.itemset((i,j), b)
+        end_time = time.process_time()
+        self.timeOneDim.setText("Time: " + str(end_time - start_time))
+        newName = 'one_dim_sigma'+str(sigma)+'.bmp'
+        cv2.imwrite(newName, img_out)
+        self.labelOne.setPixmap(newName)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+# print(timeit.timeit("main()", setup="from __main__ import main", number=1))
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
+
+
+
+
+
+
+
+
+# print(sum(sum(gauss)))
+
+# # plt.imshow(gauss, cmap=plt.get_cmap('jet'), interpolation='nearest')
+# # plt.colorbar()
+# # plt.show()
+
 
